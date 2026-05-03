4d3ba1960255f34d8d3f64018d12e5bbf841f534
Giuseppe Chiesa <mail@giuseppechiesa.it>
2026-05-03 16:33:59 +0200

    docs: fix build error with node22

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -11,9 +11,9 @@ export default defineConfig({
         src: './src/assets/ska-logo.png',
         replacesTitle: true,
       },
-      social: {
-        github: 'https://github.com/gchiesa/ska',
-      },
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/gchiesa/ska' },
+      ],
       customCss: [
         './src/styles/tailwind.css',
         './src/styles/custom.css',
